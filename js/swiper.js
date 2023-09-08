c27feb58f11056fbaab08cf684aaebedf02ac300
liucm212 <andyliu02120926@gmail.com>
2023-09-08 19:18:32 +0800

    新增diane.html & 該頁面所有components & swiper-banner

---
+++ js/swiper.js
@@ -0,0 +1,24 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper-banner', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+});
+
+
--- main.js
+++ main.js
@@ -4,5 +4,10 @@ import './assets/scss/all.scss';
 // bs collapse
 import 'bootstrap/js/dist/collapse'
 
+// swiper
+import './js/swiper'
+
+
+
 // aos animation
 import './js/aos'
