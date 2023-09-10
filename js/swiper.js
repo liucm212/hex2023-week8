30d43eb5e4787b034761db14a1a77f3be1a529f5
liucm212 <andyliu02120926@gmail.com>
2023-09-10 23:29:21 +0800

    新增index.html版面並製作RWD

--- js/swiper.js
+++ js/swiper.js
@@ -4,8 +4,8 @@ import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
 
-// init Swiper:
-const swiper = new Swiper('.swiper-banner', {
+// init banner Swiper:
+const swiperBanner = new Swiper('.swiper-banner', {
   // Optional parameters
   loop: true,
   // Navigation arrows
@@ -22,3 +22,32 @@ const swiper = new Swiper('.swiper-banner', {
 });
 
 
+// init robot swiper
+const swiperRobot = new Swiper('.swiper-robot', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+});
+
+
+
+
+
+
+// init robot swiper
+const swiperShare = new Swiper('.swiper-share', {
+  spaceBetween: 24,
+  // 滑鼠中鍵滾輪可滑動
+  mousewheel: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true
+  },
+  slidesPerView: "auto",
+});
--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import './assets/scss/all.scss';
 // bs collapse
 import 'bootstrap/js/dist/collapse'
 
+// bs corousel
+import 'bootstrap/js/dist/carousel';
+
 // swiper
 import './js/swiper'
 
